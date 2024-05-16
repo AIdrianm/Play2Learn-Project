a531547d4424e9b02e67cdca5aabcbdc7acf973a
Adrian M <montiel.merica@gmail.com>
2024-05-15 21:10:13 -0700

    addes crispy formatting to log in pages

--- play2learn/urls.py
+++ play2learn/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #Admin
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+
+    # User Management
+    path('account/', include('allauth.urls')),
+
+    # Local Apps
     path('', include('pages.urls')),
     path('pages/', include('pages.urls')),
 ]
